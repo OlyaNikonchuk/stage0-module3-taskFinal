3c13fa14fd2c39ae51a5b570b771baede93653e1
OlyaNikonchuk <olya.nikonchuk02@gmail.com>
2022-07-17 12:03:57 +0300

    first attempt

--- src/main/java/lang/print/gaps/finalModuleTask/DigitsSumCalculator.java
+++ src/main/java/lang/print/gaps/finalModuleTask/DigitsSumCalculator.java
@@ -2,5 +2,11 @@ package lang.print.gaps.finalModuleTask;
 
 public class DigitsSumCalculator {
     public void calculateSum(int number) {
+        int b,c,d,e;
+        b=number%10;
+        c=number%100/10;
+        d=number%1000/100;
+        e=number%10000/1000;
+        System.out.println(b+c+d+e);
     }
 }
--- src/main/java/lang/print/gaps/finalModuleTask/NumberReverter.java
+++ src/main/java/lang/print/gaps/finalModuleTask/NumberReverter.java
@@ -2,5 +2,10 @@ package lang.print.gaps.finalModuleTask;
 
 public class NumberReverter {
     public void revert(int number) {
+        int b,c,d;
+        b=number%10;
+        c=number%100/10;
+        d=number%1000/100;
+        System.out.println(b+""+c+""+d);
     }
 }
